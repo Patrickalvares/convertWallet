c074d2accad378b2ee944ce5e7639b5534edae82
Patrickalvares <patrickalvares@outlook.com>
2024-05-28 22:00:38 -0300

    chore: Update main screen to display currency exchange values dynamically

--- lib/features/main_screen/ui/controller/main_screen_controller.dart
+++ lib/features/main_screen/ui/controller/main_screen_controller.dart
@@ -1,19 +1,25 @@
+import 'package:flutter/foundation.dart';
+
 import '../../../../utils/helpers/log.dart';
 import '../../data/repository/main_screen_repository.dart';
 import '../../domain/entities/currency_by_currency.dart';
 
-class MainScreenController {
+class MainScreenController with ChangeNotifier {
   MainScreenController({required this.repository});
-
   final MainScreenRepository repository;
   List<CurrencyByCurrency> currencies = [];
+  bool getCurrencyValuesLoading = false;
 
   Future<void> getCurrencyValues() async {
-    await repository.getMainScreenData(params: 'USD-BRL,EUR-BRL,BTC-BRL').then((value) {
+    getCurrencyValuesLoading = false;
+    notifyListeners();
+    await repository.getMainScreenData(params: 'USD-BRL,USD-EUR,BTC-BRL,ETH-USD,LTC-EUR,XRP-BRL,DOGE-USD').then((value) {
       currencies = value;
     }).catchError((error) {});
     for (final currency in currencies) {
       Log.print('Currency: ${currency.name} ${currency.highValue}');
     }
+    getCurrencyValuesLoading = true;
+    notifyListeners();
   }
 }
--- lib/features/main_screen/ui/screens/main_screen.dart
+++ lib/features/main_screen/ui/screens/main_screen.dart
@@ -20,9 +20,83 @@ class _MainScreenState extends State<MainScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Main Screen'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Valores de Câmbio',
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.refresh),
+            color: Colors.white,
+            iconSize: 35,
+            onPressed: () {
+              widget.controller.getCurrencyValues();
+            },
+          ),
+        ],
+        backgroundColor: Colors.teal,
+      ),
+      body: AnimatedBuilder(
+        animation: widget.controller,
+        builder: (context, child) {
+          if (widget.controller.currencies.isEmpty || !widget.controller.getCurrencyValuesLoading) {
+            return const Center(
+              child: CircularProgressIndicator(
+                color: Colors.teal,
+              ),
+            );
+          }
+          return ListView.builder(
+            itemCount: widget.controller.currencies.length,
+            itemBuilder: (context, index) {
+              final currency = widget.controller.currencies[index];
+              return Card(
+                margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                child: ListTile(
+                  leading: const Icon(Icons.attach_money, color: Colors.teal),
+                  title: Text(
+                    '${currency.code}/${currency.codeIn}',
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                    ),
+                  ),
+                  subtitle: Text(
+                    currency.name,
+                    style: const TextStyle(fontSize: 12),
+                  ),
+                  trailing: Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Alta: ${currency.highValue}',
+                        style: const TextStyle(fontSize: 9),
+                      ),
+                      Text(
+                        'Baixa: ${currency.lowValue}',
+                        style: const TextStyle(fontSize: 9),
+                      ),
+                      Text(
+                        'Compra: ${currency.bid}',
+                        style: const TextStyle(fontSize: 9),
+                      ),
+                      Text(
+                        'Venda: ${currency.ask}',
+                        style: const TextStyle(fontSize: 9),
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            },
+          );
+        },
       ),
     );
   }
